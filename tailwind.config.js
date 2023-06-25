642bd07d25189556a9bbe11845539346525e8f41
Akhmed Umarov <ahmad.umarov7@gmail.com>
2023-06-25 03:23:31 +0300

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,16 +10,22 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       spacing: { 
         "7vh": "7vh",
         "9vh": "9vh",
+        "30vh": "30vh",
         "50vh": "50vh",
         "93vh": "93vh",
         "100vh": "100vh",
         "50vw": "50vw",
         "100vw": "100vw",
+      },
+      colors: { 
+        "bg-orange-infor": "#F4A900",
+        "bg-blue-math": "",
+        "bg-viol-physic": "",
       }
     },
   },
