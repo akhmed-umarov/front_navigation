aa2eebe03454eb5365042714129ea67c1261dc8c
Akhmed Umarov <ahmad.umarov7@gmail.com>
2023-07-05 00:30:36 +0300

    fixed size

--- src/components/thema-block/thema-block.tsx
+++ src/components/thema-block/thema-block.tsx
@@ -7,10 +7,10 @@ const ThemaBlock = ({ description, imgUrl }: { description: string, imgUrl: stri
         <div className='flex flex-col justify-center items-center m-3'>
             <p className='text-justify'>{description}</p>
             {imgUrl == '' || imgUrl == undefined ? null :
-            <Image className='rounded-sm w-1/2 h-1/3 my-2' width={100} height={100} alt='Изображение темы'  src={`${API_URL}/uploads/${imgUrl}`} />
+            <Image className='rounded-sm w-3/4 h-1/2 my-2' width={100} height={100} alt='Изображение темы'  src={`${API_URL}/uploads/${imgUrl}`} />
             }
         </div>
     );
 };
 
-export default ThemaBlock;
+export default ThemaBlock
